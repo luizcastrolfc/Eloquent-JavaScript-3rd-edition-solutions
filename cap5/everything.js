774b24d379cec2deae876e8a9269a6ddbac26c2a
Luiz Castro <fellipeluizz@gmail.com>
2018-04-20 15:47:03 -0300

    everything

---
+++ cap5/everything.js
@@ -0,0 +1,25 @@
+function every(array, test) {
+  // Your code here.
+  for (let item of array)
+    if(!test(item))
+      return false;
+  return true;
+}
+
+const everyWithSome = (array, test) => {
+  return !array.some(item => !test(item)); 
+}
+
+console.log(everyWithSome([1, 3, 5], n => n < 10));
+// → true
+console.log(everyWithSome([2, 4, 16], n => n < 10));
+// → false
+console.log(everyWithSome([], n => n < 10));
+// → true
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
